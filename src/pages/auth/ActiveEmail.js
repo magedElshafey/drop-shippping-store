32a3fc321ee26e13b9d0d43279dee1af8a78e5fb
magedElshafey <magedelshafey98@gmail.com>
2024-09-25 12:58:13 +0300

    create project structure

---
+++ src/pages/auth/ActiveEmail.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ActiveEmail = () => {
+  return <div>ActiveEmail</div>;
+};
+
+export default ActiveEmail;
---
+++ src/pages/auth/ActiveForgetPassword.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ActiveForgetPassword = () => {
+  return <div>ActiveForgetPassword</div>;
+};
+
+export default ActiveForgetPassword;
