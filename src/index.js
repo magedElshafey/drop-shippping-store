ecb465c38da52ec73d256e957f5fb4eaa09c0cfa
magedElshafey <magedelshafey98@gmail.com>
2024-09-25 13:45:46 +0300

    configure i18next to the project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// style
 import "./style/index.css";
+// routes
 import AppRouter from "./router/AppRouter";
+// i18n
+import "./i18n/i18n";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<AppRouter />);
