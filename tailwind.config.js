995a4e2de5a515a57e87b84c5d62f11714dbd3e1
magedElshafey <magedelshafey98@gmail.com>
2024-09-25 14:14:23 +0300

    add common style to index.css file , add the font to the body and finally , adding the variables to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        maincolorgreen: "#00AA4B",
+        secondcolorgreen: "#007D56",
+        grayColor: "#F0F2F0",
+        textColor: "#7a8499",
+        yellow: "#ffedce",
+        mint: "#c5f4df",
+        pink: "#fed6db",
+      },
+    },
   },
   plugins: [],
 };
